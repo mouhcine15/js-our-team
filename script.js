d7f07846f1bc5d4e08fb16dfbc62beb013ade8f9
mouhcine <mouhcine15.00@gmail.com>
2022-05-18 15:54:14 +0200

    creazione oggetti

---
+++ script.js
@@ -0,0 +1,49 @@
+
+let card = document.getElementsByClassName("team-card");
+
+
+const oggetti = 
+[
+    {
+        "nome" : "Wayne Barnett",
+        "ruolo" : "Founder & CEO",
+        "img.src" : "img/wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        "nome" : "Angela Caroll",
+        "ruolo" : "Chief editor",
+        "img.src" :  "img/angela-caroll-chief-editor.jpg"
+    },
+
+    {
+        "nome" : "Walter Gordon",
+        "ruolo" : "Office Manager",
+        "img.src" :  "img/walter-gordon-office-manager.jpg"
+    },
+
+    {
+        "nome" : "Angela Lopez",
+        "ruolo" : "Social Media Manager",
+        "img.src" :  "img/angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+        "nome" : "Scott Estrada",
+        "ruolo" : "Developer",
+        "img.src" :  "img/scott-estrada-developer.jpg"
+    },
+
+    {
+        "nome" : "Barbara Ramos",
+        "ruolo" : "Graphic Designer",
+        "img.src" :  "img/barbara-ramos-graphic-designer.jpg"
+    }
+
+];
+
+
+ console.log(oggetti);
+
+// document.getElementsByClassName("team-card").innerHTML = azienda;
+// card.innerHTML = azienda;
